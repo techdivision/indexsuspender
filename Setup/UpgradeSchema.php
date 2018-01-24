e282d17e97f45dcf9c2a74a512431955d511243a
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2018-01-24 14:59:53 +0100

    clean suspender for invalid indexer

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) 2017 TechDivision GmbH
+ * All rights reserved
+ *
+ * This product includes proprietary software developed at TechDivision GmbH, Germany
+ * For more information see http://www.techdivision.com/
+ *
+ * To obtain a valid license for using this software please contact us at
+ * license@techdivision.com
+ */
+
+namespace TechDivision\IndexSuspender\Setup;
+
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use TechDivision\IndexSuspender\Api\Constants;
+
+/**
+ * Class UpgradeSchema
+ *
+ * @package     TechDivision\IndexSuspender\Setup
+ * @file        InstallSchema.php
+ * @copyright   Copyright (c) 2017 TechDivision GmbH (http://www.techdivision.com)
+ * @site        https://www.techdivision.com/
+ * @author      Martin Eisenführer <m.eisenfuehrer@techdivision.com>
+ */
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '0.3.0', '<')) {
+            $this->addDatetimeColumnToSuspenderTable($setup);
+        }
+        $setup->endSetup();
+    }
+
+    /**
+     * @param SchemaSetupInterface $setup
+     * @return void
+     */
+    protected function addDatetimeColumnToSuspenderTable(SchemaSetupInterface $setup)
+    {
+        $setup->getConnection()->addColumn(
+            $setup->getTable(Constants::DB_TABLE_NAME),
+            'created_at',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT,
+                'comment' => 'Created At'
+            ]
+        );
+    }
+}
